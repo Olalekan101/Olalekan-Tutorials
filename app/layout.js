029a7e63d598833f7a1804dc5f469f528e6fb995
Lekan <38636712+Ginkk@users.noreply.github.com>
2023-04-13 20:06:04 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Navbar from './Navbar/page'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from "react-query/devtools"
 import { sono } from 'next/font/google';
+import { Roboto_Mono } from "next/font/google"
 import Footer from './Footer/page'
 import TerminologyProvider from '@/lib/ContextApi/TerminologyContex'
 import CheckBoxContext from './ContextAPI/CheckBoxContext'
@@ -13,6 +14,11 @@ import CheckBoxContext from './ContextAPI/CheckBoxContext'
 //   subsets: ['latin'],
 //   display: 'swap',
 // });
+
+const robo = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 const queryClient = new QueryClient()
 // export const revalidate = 0
@@ -24,7 +30,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='sono.className' >
+    <html lang="en" className={robo.className} >
       <body className={` bgText flex flex-col min-h-screen p-2 sm:p-6 `}>
       <QueryClientProvider client={queryClient}>
         <CheckBoxContext>
